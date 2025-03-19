a28aa00af82bc5ed5ff2bb9e9c69cb47aa1e905a
mke5 <emma08062602618@gmail.com>
2025-03-19 15:13:33 +0100

    CLI tool Implementation

--- app/sh/Sh.php
+++ app/sh/Sh.php
@@ -1,13 +1,36 @@
 <?php 
 
-namespace app\sh;
+namespace App\Sh;
 
 defined('CPATH') or exit('No direct script access allowed');
 
 class Sh
 {
-    public function __construct()
+    private $version = '1.0.0';
+    public function make()
     {
         echo 'Sh class';
+    }
+
+    public function help() {
+
+        echo "
+        
+            Sh v$this->version Command Line Interface
+
+            Database:
+                db:create               Create a new database
+                db:refresh              Reset and re-run all migrations and seeds
+                db:drop                 Drop a database
+                db:migrate              Run all pending migrations
+                db:seed                 Seed the database with records
+                db:rollback             Rollback the last database migration
+                db:status               Show the status of each migration
+
+
+            Migrations:
+                make:migration             Create a new migration file
+                make:model                 Create a new model file
+        ";
     }
 }
--- sh
+++ sh
@@ -4,8 +4,18 @@ if(PHP_SAPI !== 'cli') {
     die('This script can only be run from the command line.');
 }
 
+define('DS', DIRECTORY_SEPARATOR);
 define('CPATH', __DIR__.DIRECTORY_SEPARATOR);
 chdir(CPATH);
 
 $action = $argv[1] ?? 'help';
 
+require_once CPATH."app".DS."sh".DS."init.php";
+
+$sh = new App\Sh\sh;
+
+if(empty($action)) {
+    call_user_func_array([$sh, 'help'], []);
+} else {
+    call_user_func_array([$sh, $action],[]);
+}
